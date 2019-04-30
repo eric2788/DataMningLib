4c98cdeb432a21a6e9af3e2bab7571941f969fc3
Lam <tsukiko852@gmail.com>
2019-05-01 03:16:36 +0800

    added association rule testing in main

--- src/com/ericlam/sorting/Main.java
+++ src/com/ericlam/sorting/Main.java
@@ -2,17 +2,50 @@ package com.ericlam.sorting;
 
 import com.ericlam.sorting.sortfunc.Histogram;
 import com.ericlam.sorting.sortfunc.QuantileSort;
+import com.ericlam.sorting.spreading.mining.Association;
+import com.ericlam.sorting.utils.AdvMath;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {85, 60, 89, 97, 98, 94, 83, 69, 89, 94, 90, 78, 88, 102, 82, 110, 90, 90, 80, 68, 89, 94, 81, 65, 102, 86, 84, 89, 92, 72, 82, 94, 81, 80, 91, 85, 86, 103, 85, 93,
-                82, 60, 81, 94, 98, 79, 83, 86, 75, 86, 94, 85, 90, 64, 105, 96, 82, 95, 99, 82, 102, 77, 75, 77, 77, 98, 87, 73, 73, 85, 76, 94, 73, 60, 99, 106, 94, 89, 79, 112, 94,
-                82, 75, 86, 79, 82, 92, 94, 84, 93, 60, 82, 71, 89, 110, 74, 98, 96, 106};
-        double[] q1 = {1,3,5,7,9,10};
-        double[] q2 = {2,4,6,8,10};
+
+        String[] row1 = {"Bread", "Milk"};
+        String[] row2 = {"Bread", "Diaper", "Beer", "Eggs"};
+        String[] row3 = {"Milk", "Diaper", "Beer", "Cake"};
+        String[] row4 = {"Bread", "Milk", "Diaper", "Beer"};
+        String[] row5 = {"Bread", "Milk", "Diaper", "Cake"};
+
+        String[] if1 = {"Milk", "Diaper"};
+        String[] then1 = {"Beer"};
+
+        String[] if2 = {"Beer", "Bread"};
+        String[] then2 = {"Milk"};
+
+        double[][] tables = new double[][]{
+                AdvMath.toHashDoubleArray(row1),
+                AdvMath.toHashDoubleArray(row2),
+                AdvMath.toHashDoubleArray(row3),
+                AdvMath.toHashDoubleArray(row4),
+                AdvMath.toHashDoubleArray(row5),
+        };
+
+        Association ass = new Association(tables, 0.5, 0.4);
+        checkAssociation(ass, if1, then1);
+        checkAssociation(ass, if2, then2);
+
     }
 
-    public static void QuantilePlot(int[] test){
+    private static void checkAssociation(Association ass, Object[] if1, Object[] then1) {
+        double conf1 = ass.getConfidence(AdvMath.toHashDoubleArray(if1), AdvMath.toHashDoubleArray(then1));
+        double sup1 = ass.getSupport(AdvMath.toHashDoubleArray(if1), AdvMath.toHashDoubleArray(then1));
+        System.out.println(Arrays.toString(if1) + " => " + Arrays.toString(then1));
+        System.out.println("Conf: " + AdvMath.round(2, conf1));
+        System.out.println("Sup: " + AdvMath.round(2, sup1));
+        System.out.println("Passed: " + ass.implement(AdvMath.toHashDoubleArray(if1), AdvMath.toHashDoubleArray(then1)));
+    }
+
+    private static void QuantilePlot(int[] test) {
         QuantileSort sort = new QuantileSort(test);
         System.out.println("After Sorted:"+sort.getList());
         for (QuantileSort.Type type : QuantileSort.Type.values()) {
@@ -26,7 +59,7 @@ public class Main {
         sort.printGraph1(true);
     }
 
-    public static void Histogram(int[] test){
+    private static void Histogram(int[] test) {
         Histogram his = new Histogram(test);
         his.printGraph();
     }
